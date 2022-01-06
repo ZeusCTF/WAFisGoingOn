bcebea64470e09dd9389e43700dd098a8281cef0
Barry Allen <barryallen@Barrys-MacBook-Air.local>
2022-01-05 20:32:03 -0500

    cleaned up code, added POST functionality

--- app.py
+++ app.py
@@ -5,8 +5,6 @@ import requests
 from requests.sessions import Request
 
 app = Flask(__name__)
-
-
 '''
 Gather URL params
 https://stackoverflow.com/questions/15974730/how-do-i-get-the-different-parts-of-a-flask-requests-url
@@ -16,29 +14,20 @@ Returning a response object?
 https://stackoverflow.com/questions/19568950/return-a-requests-response-object-from-flask
 '''
 
-#create a model of the web server env:
-
-
-
-
-
 def info_gather(var):
     if request.method == 'GET':
         url = 'http://127.0.0.1:8080/' + var
         r = requests.get(url)
         return r.text
+    elif request.method == 'POST':
+        url = 'http://127.0.0.1:8080/' + var
+        r = requests.post(url, data=request.form.to_dict(flat=False))
+        return r.text
 
-        #so we can return a redirect to the external URL with `redirect(url)` but this redirects them to the site, instead of having them submit requests through the proxy
-        #return redirect(url)
-
-
-
-@app.route("/<reqPath>")
+@app.route("/<reqPath>", methods=['GET','POST'])
 def render(reqPath):
     return info_gather(reqPath)
-    
 
-    
 #runs the app without having to set any of the env vars that are listed in the documentation
 if __name__ == '__main__':
     app.run(debug=True)
